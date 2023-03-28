e4f42143c2b572e025d168a076b420867e9bc72a
krzysiek581234 <krzysztof.madajczakbiznes@gmail.com>
2023-03-28 23:58:20 +0200

    progess in projekt from stock analizer

---
+++ Platformy technologiczne/C#/lab5/lab5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    class Program
+    {
+        public static int zad1a(int a, int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += a;
+                a = a * 2;
+            }
+
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Task<int> taskD = new Task<int>(() => zad1a(2, 10));
+            taskD.Start();
+            taskD.Wait();
+            Console.WriteLine(taskD.Result);
+            Console.WriteLine("aa");
+        }
+
+
+    }
+}
